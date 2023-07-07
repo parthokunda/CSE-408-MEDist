1fbfb588eb1422c9d0ed515ae26c330d3a473536
Udayon-Paul-Dhrubo <1805109@ugrad.cse.buet.ac.bd>
2023-07-07 20:18:53 +0600

    logger added

--- Web-Scrapping/src/server.ts
+++ Web-Scrapping/src/server.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+import log from "./utils/logger";
+
+log.info("Hello World!");
+log.error("This is an error!");
+log.warn("This is a warning!");
+log.debug("This is a debug message!");
+log.trace("This is a trace message!");
