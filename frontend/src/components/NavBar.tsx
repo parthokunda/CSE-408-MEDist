b1bc13e7a9126f811122d19a9af589875f0d4a2f
piratepartho <parthokunda@gmail.com>
2023-07-30 01:34:12 +0600

    navbar banaite din par. ggez

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+import { navIcon } from "../models/navIcon";
+
+const NavBar: FC<{ navList: navIcon[] }> = (props) => {
+  return (
+    <NavigationMenu>
+      <div className="flex items-center w-screen h-14 bg-c1 font-inter ">
+        <div className="font-bold text-2xl text-white ml-4 flex-grow">
+          MEDist
+        </div>
+        <NavigationMenuList className="flex px-4 gap-4">
+          {props.navList.map((icon) => (
+            <NavigationMenuItem key={icon.name} className="text-c4 hover:text-white">
+              <NavigationMenuLink href={icon.link? icon.link : ""}>{icon.name}</NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </div>
+    </NavigationMenu>
+  );
+};
+
+export default NavBar;
