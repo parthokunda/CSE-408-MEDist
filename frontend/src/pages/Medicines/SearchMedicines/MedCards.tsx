2443f1e631b8c760030563dabe8746d248b981e0
piratepartho <parthokunda@gmail.com>
2023-08-06 14:04:00 +0600

    description page grid and page number nav ui update

--- frontend/src/pages/Medicines/SearchMedicines/MedCards.tsx
+++ frontend/src/pages/Medicines/SearchMedicines/MedCards.tsx
@@ -2,69 +2,85 @@ import { BrandInfo } from "@/models/Brand";
 import { FC } from "react";
 import MedCard from "./MedCard";
 import { useState } from "react";
-import {AiOutlineArrowLeft} from "react-icons/ai";
-import {AiOutlineArrowRight} from "react-icons/ai";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { AiOutlineArrowRight } from "react-icons/ai";
 const MedCards: FC<{
   brandFetchedData: BrandInfo[];
 }> = (props) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const recordsPerPage = 12;
+  const recordsPerPage = 15;
   const lastIndex = currentPage * recordsPerPage;
   const firstIndex = lastIndex - recordsPerPage;
   const records = props.brandFetchedData.slice(firstIndex, lastIndex);
   const nPages = Math.ceil(props.brandFetchedData.length / recordsPerPage);
-  // const pages = [...Array(nPages).keys()].map((i) => i + 1);
-  //3pages around currentPage
-  // const threePages = (currentPage: number) => {
-  //   if (currentPage === 1) {
-  const array=[currentPage-2,currentPage-1,currentPage,currentPage+1,currentPage+2]
+  const array = [
+    currentPage - 2,
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    currentPage + 2,
+  ];
   const pages = array.filter((page) => page > 0 && page <= nPages);
-  // const pages_3 = [...Array(3).keys()].map((i) => i + 1);
   if (props.brandFetchedData && props.brandFetchedData.length === 0) {
     return (
       <div className="flex justify-center align-middle">No Item Found</div>
     );
   }
   return (
-    <div>
-      <div className="flex flex-wrap mt-3 gap-6">
+    <div className="">
+      <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 mt-3 gap-5">
         {props.brandFetchedData &&
           records.map((medicine) => (
             <MedCard medicine={medicine} key={medicine.Brand.id} />
           ))}
       </div>
-      <nav>
+      <nav className="absolute bottom-0 w-full pb-3">
         <ul className="flex items-center justify-center">
-          {
-              currentPage!==1 && <li className="ml-2 mr-4  hover:text-c2">
-              <a className="page-link" href="#" onClick={prevPage}>
-                <AiOutlineArrowLeft/>
+          {currentPage !== 1 && (
+            <li
+              className="px-2 py-2 text-xl hover:rounded-full hover:text-white hover:bg-c1 duration-300"
+              onClick={prevPage}
+            >
+              <a className="page-link">
+                <AiOutlineArrowLeft />
               </a>
             </li>
-          }
-          
+          )}
+          {currentPage === 1 && (
+            <li className="px-2 py-2 text-xl text-opacity-30 hover:rounded-full hover:text-white hover:bg-gray-400 duration-300">
+              <a className="page-link" aria-disabled>
+                <AiOutlineArrowLeft />
+              </a>
+            </li>
+          )}
           {pages.map((page) => (
-            <li
-              className={
-                page === currentPage
-                  ? "text-c1 font-bold mr-4 hover:text-c2 "
-                  : "mr-4 hover:text-c2 "
-              }
-            >
-              <a className="page-link" href="#" onClick={() => setPage(page)}>
+            <li>
+              <a
+                className={
+                  page === currentPage
+                    ? "px-4 py-2 text-xl text-white rounded-full bg-c3 font-bold "
+                    : "px-4 py-2 text-xl hover:rounded-full hover:bg-c1 hover:text-white duration-300"
+                }
+                onClick={() => setPage(page)}
+              >
                 {page}
               </a>
             </li>
           ))}
-          {
-            currentPage!==nPages &&
-            <li className="mr-4 hover:text-c2">
-            <a className="page-link" href="#" onClick={nextPage}>
-              <AiOutlineArrowRight/>
-            </a>
-          </li>
-          }
-          
+          {currentPage !== nPages && (
+            <li className="px-2 py-2 text-xl hover:rounded-full hover:text-white hover:bg-c1 duration-300">
+              <a className="page-link" onClick={nextPage}>
+                <AiOutlineArrowRight />
+              </a>
+            </li>
+          )}
+          {currentPage === nPages && (
+            <li className="px-2 py-2 text-xl text-opacity-30 hover:rounded-full hover:text-white hover:bg-gray-400 duration-300">
+              <a className="page-link" aria-disabled>
+                <AiOutlineArrowRight />
+              </a>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
