ca4922730cc742f1260d76092101f7acc99c077a
Udayon-Paul-Dhrubo <1805109@ugrad.cse.buet.ac.bd>
2023-08-08 16:38:07 +0600

    cloud database activated in auth and patient service will do next for medicine service

--- Auth_Service/src/database/config.ts
+++ Auth_Service/src/database/config.ts
@@ -1,19 +1,26 @@
 // external import
 import { Dialect, Sequelize } from "sequelize";
+require("dotenv").config();
 
 // internal import
-import { config } from "../config";
+// import { config } from "../config";
 
-const dbName = config.DB.NAME;
-const dbUser = config.DB.USER;
-const dbHost = config.DB.HOST;
-const dbDriver = config.DB.DRIVER;
-const dbPassword = config.DB.PASSWORD;
+// const dbName = config.DB.NAME;
+// const dbUser = config.DB.USER;
+// const dbHost = config.DB.HOST;
+// const dbDriver = config.DB.DRIVER;
+// const dbPassword = config.DB.PASSWORD;
 
-const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
-  dialect: dbDriver,
-  logging: false,
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+const sequelizeConnection = new Sequelize(URL, {
+  dialect: "postgres" as Dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
 
 export default sequelizeConnection;
